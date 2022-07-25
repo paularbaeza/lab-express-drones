e9345b92727c10f8e64846ef8173c193d399209d
Malva Prior <malvacs@gmail.com>
2022-07-25 18:09:55 +0200

    interacción 4

--- routes/drones.js
+++ routes/drones.js
@@ -20,25 +20,56 @@ router.get('/drones', (req, res, next) => {
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form.hbs") 
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const {name, propellers, maxSpeed } = req.body
+  Drone.create({
+    name,
+    propellers,
+    maxSpeed
+  })
+  .then (() =>{
+    res.redirect("/drones")
+  })
+  .catch((err)=>{
+    next(err)
+  })
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:droneId/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {droneId} = req.params
+
+  Drone.findById(droneId)
+  .then ((droneInfo) =>{
+    res.render("drones/update-form.hbs", {droneInfo,})
+  })
+  .catch((err)=>{
+    next(err)
+  })
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:droneId/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const {droneId} = req.params
+  const {name, propellers, maxSpeed} = req.body
+  Drone.findByIdAndUpdate(droneId, {
+    name,
+    propellers,
+    maxSpeed
+  })
+  .then (() => {
+    res.redirect("/drones")
+  })
+  .catch((err)=>{
+    next(err)
+  })
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:droneId/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
