dd2bc070c0fb80b90304a5f10cfc3c55c2784666
Paula Rodríguez <paularb923@hotmail.com>
2022-07-25 15:59:12 +0100

    iteration 1 y 2

--- routes/drones.js
+++ routes/drones.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 // require the Drone model here
+const Drone = require("../models/Drone.model.js");
+
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+ Drone.find()
+ .then ((droneInfo)=> {
+  res.render("drones/list.hbs",{
+    droneInfo
+  })
+ })
+ .catch((err)=> {
+  next(err)
+ })
 });
 
 router.get('/drones/create', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,27 @@
 // Iteration #1
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+//requerir la conexion
+const mongoose = require("mongoose")
+require("../db")
+
+//traer el modelo
+const Drone = require ("../models/Drone.model")
+
+//metodo 
+async function storeDrones () {
+try {
+    await Drone.create(drones)
+    console.log ("drones agregados a la BD")
+    mongoose.connection.close()
+}
+catch (err){
+    console.log(err)
+}
+}
+
+storeDrones()
