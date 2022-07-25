4dbdc3884b1a1fd2d7fe59283cc2af46b101ded8
Paula Rodríguez <paularb923@hotmail.com>
2022-07-25 18:22:13 +0100

    styles added

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -2,7 +2,10 @@
 const drones = [
     { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
     { name: "Racer 57", propellers: 4, maxSpeed: 20 },
-    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+    { name: "Malva 704xI", propellers: 8, maxSpeed: 24 },
+    { name: "Paula hyehXi", propellers: 5, maxSpeed: 18 },
+    { name: "Jose BetisShcy3", propellers: 7, maxSpeed: 15 },
   ];
 
 //requerir la conexion
